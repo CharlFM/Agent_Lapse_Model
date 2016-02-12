9b75a4e0972da28c22cd2b751c1b86c966a2be3a
Charl Marais <charl.marais@zestlife.co.za>
2016-02-12 17:54:47 +0200

    Fake Loop added

--- Main.R
+++ Main.R
@@ -36,6 +36,22 @@ source(paste(Path, "/R_Code/Appender.R", sep = ""))
 # Prepare Model Data #
 ######################
 
+AgentDat <- Low5LAP[2] # Low5LAP ; Top5LAP ; Low5NTU ; Top5NTU
+AgentDat
+Dat_Loop <- LAPData[LAPData$AGENTNAME == AgentDat, ]
+Dat_Loop <- subset(Dat_Loop, select = -c(AGENTNAME, Year))
+to_drop <- c()
+
+for (i in 1:ncol(Dat_Loop)) {
+  if (sum(Dat_Loop[[i]] == Dat_Loop[[i]][1]) == nrow(Dat_Loop)) {
+    to_drop <- c(to_drop, i)
+  }
+}
+
+Dat_Loop <- Dat_Loop[, -to_drop]
+
+prop.table(table(Dat_Loop$STATUS))
+
 source(paste(Path, "/R_Code/Prep_Model_Data.R", sep = ""))
 
 #####################################################################################################
@@ -50,9 +66,8 @@ source(paste(Path, "/R_Code/Build_Model.R", sep = ""))
 
 source(paste(Path, "/R_Code/Testing.R", sep = ""))
 
-
-
-
+impdf
+print(auc$auc)
 
 
 
