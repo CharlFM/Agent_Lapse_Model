0269330f1824fb9f36edc974e55ee93b3e9772d6
Charl Marais <charl.marais@zestlife.co.za>
2016-02-12 17:56:10 +0200

    Adding variable importance

--- R_Code/Build_Model.R
+++ R_Code/Build_Model.R
@@ -1,6 +1,6 @@
 
 objControl <- trainControl(method        =  "cv",
-                           number        =  3, 
+                           number        =  4, 
                            returnResamp  =  "none")
 
 objModel <- train(x            =  trainDF[, predictorsNames], 
@@ -9,19 +9,7 @@ objModel <- train(x            =  trainDF[, predictorsNames],
                   metric       =  "RMSE", 
                   trControl    =  objControl)
 
-predictions <- predict(objModel, testDF[, predictorsNames])
 
-auc <- roc(testDF[,outcomeName], predictions)
-print(auc$auc)
-plot(auc)
-
-imp    <-  varImp(objModel, scale = F)
-impdf  <-  imp$importance
-Vars   <-  rownames(impdf)
-Vals   <-  impdf$Overall
-impdf  <-  data.frame(Vars, Vals)
-impdf  <-  data.frame(impdf[impdf$Vals != 0, ])
-impdf  <-  impdf[with(impdf, order(-Vals)), ]
 
 
 
